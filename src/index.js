a83d5acbde256f73c227e50a7be00d917aa214fe
thienphan <thienkphan@gmail.com>
2020-07-15 12:11:37 +0700

    Add redux-saga for side effects

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import configurableStore, { history } from '@/store';
+import configurableStore, { history, sagaMiddleware } from '@/store';
 import { ConnectedRouter } from 'connected-react-router';
+import saga from '@/store/saga';
+
 import App from './App.jsx';
 import 'semantic-ui-css/semantic.min.css';
 import '@styles/index.styl';
 
 const store = configurableStore();
+sagaMiddleware.run(saga);
 ReactDOM.render(
 	// eslint-disable-next-line react/jsx-filename-extension
 	<Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -4,17 +4,25 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createBrowserHistory } from 'history';
+import createSagaMiddleware from 'redux-saga';
 import { routerMiddleware } from 'connected-react-router';
 import reducer from './reducers.js';
 
 const logger = createLogger({});
+export const sagaMiddleware = createSagaMiddleware();
 export const history = createBrowserHistory();
+
+// const customMiddleware = (store) => (next) => (action) => {
+// 	const result = next(action);
+// 	console.log(store.getState());
+// 	console.log(result, action);
+// };
 
 function configurableStore() {
 	return createStore(
 		reducer(history),
 		undefined,
-		compose(applyMiddleware(routerMiddleware(history), thunk, logger), composeWithDevTools())
+		compose(applyMiddleware(routerMiddleware(history), thunk, sagaMiddleware, logger), composeWithDevTools())
 	);
 }
 
